3f5ac9ebe12d25d711fff1ba518500f62ad1f36a
oschvr <oschvr@protonmail.com>
2025-11-25 12:39:31 +0100

    Cloud Storage

---
+++ clouds/gcp/cloud-storage/main.tf
@@ -0,0 +1,22 @@
+# main.tf
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = "europe-west2-a"
+}
+
+resource "google_storage_bucket" "instantinfra" {
+  name = "${var.prefix}-gcs"
+  location = upper(var.region)
+  force_destroy = true
+}
+
